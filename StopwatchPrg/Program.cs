1c4f8cb42521c0ef763569c21b049dfe0f18f05e
unknown <khapareamol@gmail.com>
2020-08-29 22:16:36 +0530

    Initial Commit

---
+++ StopwatchPrg/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StopwatchPrg
+{
+    class StopWatchModel
+    {
+       
+        static void Main(string[] args)
+        {
+            int choice;
+            StopWatch SW = new StopWatch();
+            
+            
+                Console.WriteLine("1. Start");
+                Console.WriteLine("2. Stop");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("Enter Your Choice:");
+            do
+            {
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        SW.Start();
+                        break;
+                    case 2:
+                        SW.Stop();
+                        Console.WriteLine("Time Span:{0}",SW.GetInterval());
+                        break;
+                    case 3: System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect Choice. Please try again...");
+                        break;
+                }
+            } while(true);
+
+            
+            
+        }
+    }
+}
---
+++ StopwatchPrg/StopWatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StopwatchPrg
+{
+    class StopWatch
+    {
+        DateTime startTime { get; set; }
+        DateTime stopTime { get; set; }
+        bool flag = false;
+        public void Start()
+        { 
+            if (flag == false)
+            {
+                startTime = DateTime.Now;
+                flag = true;
+            }
+        }
+        public void Stop()
+        {   
+            if (flag == true)
+            {
+                stopTime = DateTime.Now;
+                flag = false;
+            }
+        }
+        public TimeSpan GetInterval()
+        {
+            return(stopTime-startTime);
+        }
+    }
+}
